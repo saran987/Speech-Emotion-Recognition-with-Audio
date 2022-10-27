15783d2bbb3711bc8b8571d73929c46412a9c815
Sai Saran Akkiraju <sai.saran987@gmail.com>
2022-10-27 11:17:24 -0400

    Update DataLoad.py

--- DataLoad.py
+++ DataLoad.py
@@ -6,9 +6,11 @@ def data_Load():
     processed_data_path= r'Speech-Emotion-Recognition-with-Audio/Processed_Data.csv'
 
     df=pd.read_csv(processed_data_path)
-
+    
+    #Removing Null Values
     df=df.fillna(0)
 
+    #Splitting Data as X and Y for training and testing
     X=df.drop(labels='Emotion',axis=1)
 
     Y=df['Emotion']
